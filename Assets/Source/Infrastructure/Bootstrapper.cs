136e25efc20bf0f1bfe1abeadf119edd20648f13
zelenin.m <irbissssss@net.com>
2025-10-16 22:00:58 +0300

    Add zenject

---
+++ Assets/Source/Infrastructure/Bootstrapper.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Source.Infrastructure
+{
+    public class Bootstrapper : MonoBehaviour
+    {
+        [SerializeField] BootObjectsDataBase _bootObjectsData;
+
+        private void Awake()
+        {
+            InittializeScene();
+        }
+
+        private void InittializeScene()
+        {
+
+        }
+    }
+}
---
+++ Assets/Source/Infrastructure/Scriptables/BootObjectsData.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public struct BootObjectsData
+{
+    public string Tag;
+    public GameObject Prefab;
+    public Vector3 Position;
+}
